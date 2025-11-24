5c449349918ec7fe29cde83a2171b91845509f9d
Benicio Lovato <beniciol164@alu.bue.edu.ar>
2025-11-24 17:50:41 -0300

    Agrego mas publicidad

--- views/partidos.php
+++ views/partidos.php
@@ -14,6 +14,10 @@
     
     <?php include 'header.php'; ?>
 
+<div class="publicidad">
+        <a href="https://maples.com/directorship-and-fund-governance-services?utm_source=bing&utm_medium=display&utm_campaign=486898705&utm_content=1227056218999618&utm_term=c&utm_campaign=486898705&utm_medium=display&utm_source=bing&utm_term=c&utm_content=1227056218999618&msclkid=9ec6376a66ef1ca0188c5e4eebb02b36"><img src="../img/publicidad3.jpg" alt="Publicidad"></a>
+    </div>
+
 <div class="container">
     <h2>PARTIDOS DE HOY</h2>
 
